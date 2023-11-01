043ebd43a06dd1d80a05597fb0d3abad9d3ee1fb
Devansh Rajput <rajputdevansh012@gmail.com>
2023-11-01 23:11:48 +0530

    Update file

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import session from 'express-session'
 import cors from 'cors'
 import connectDB from './config/connectDb.js'
-import Routes from './Route.js'
+// import Routes from './Route.js'
 
 const app = express()
 const port = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
+
+app.set("view engine", "ejs");
+
+app.get("/login",(req,res)=>{
+  res.render("auth/login.ejs")
+})
 
 //Cors Policy
 app.use(cors())
@@ -19,8 +26,8 @@ connectDB(DATABASE_URL)
 app.use(express.json())
 
 // Load Routes
-app.use("/api/user", Routes)
+// app.use("/api/user", Routes)
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-  })
+  console.log(`Server listening at http://localhost:${port}`)
+})
--- auth/auth.js
+++ auth/auth.js
@@ -1,15 +1,42 @@
 const passport = require('passport');
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GoogleStrategy = require( 'passport-google-oauth20').Strategy;
+import ejs from 'ejs'
+import path from 'path'
+
+app.use(
+  session({
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://yourdomain:3000/auth/google/callback",
-    passReqToCallback   : true
+    callbackURL: "http://localhost:8000/auth/google/callback",
   },
-  function(request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
+  function(accessToken, refreshToken, profile, cb) {
+    cb(null, profile);
   }
 ));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/login", (req, res) => {
+  res.render(path.join(__dirname, "auth/login.ejs"));
+});
